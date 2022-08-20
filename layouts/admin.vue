49e09fa2820a4019ce3dcce6488f4eacc32dc694
Nazar Koval <nazarkoval04@gmail.com>
2022-08-20 21:28:21 +0300

    added: admin page

---
+++ layouts/admin.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="wrapper">
+      <Header />
+      <div class="wrapper-content wrapper-content--fixed">
+         <Intro title="Admin Page">
+            <nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
+         </Intro>
+         <nuxt />
+      </div>
+   </div>
+</template>
+
+<script>
+import Header from "@/components/system/Header.vue";
+export default {
+   components: {
+      Header,
+   },
+};
+</script>
+
+<style lang="scss"></style>
