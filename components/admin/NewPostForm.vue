d45b136553cd4d626fb0c04a0f617bec212ba941
Nazar Koval <92331274+Liknox@users.noreply.github.com>
2022-08-21 00:37:48 +0300

    Koval3 (#9)
    * created: admin form to create posts
    
    * created: buttons to cancel or save "new post"
    
    * created: restrict editing only for admins

---
+++ components/admin/NewPostForm.vue
@@ -0,0 +1,56 @@
+<template>
+   <section class="new-post">
+      <div class="container">
+         <form @submit.prevent>
+            <AppInput v-model="post.title">Title:</AppInput>
+            <AppInput v-model="post.descr">Description:</AppInput>
+            <AppInput v-model="post.img">Img Link:</AppInput>
+            <AppTextArea v-model="post.content">Content: </AppTextArea>
+
+            <div class="controls">
+               <AppButton class="btnDanger" @click="cancel">Cancel</AppButton>
+               <AppButton @click="onSubmit">Save</AppButton>
+            </div>
+         </form>
+      </div>
+   </section>
+</template>
+
+<script>
+export default {
+   props: {
+      postEdit: {
+         type: Object,
+         required: false,
+      },
+   },
+   data() {
+      return {
+         post: this.postEdit
+            ? { ...this.postEdit }
+            : {
+                 title: "",
+                 descr: "",
+                 descr: "",
+                 img: "",
+                 content: "",
+              },
+      };
+   },
+   methods: {
+      cancel() {
+         this.$router.push("/admin");
+      },
+      onSubmit() {
+         this.$emit("submit", this.post);
+      },
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+.controls {
+   text-align: center;
+   margin: 50px 0;
+}
+</style>
