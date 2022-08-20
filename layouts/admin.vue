ad746b0da59243da137ed234300fd8a029439ab0
Nazar Koval <nazarkoval04@gmail.com>
2022-08-20 21:38:39 +0300

    created: new-post page for admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -4,6 +4,7 @@
       <div class="wrapper-content wrapper-content--fixed">
          <Intro title="Admin Page">
             <nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
+            <nuxt-link to="/admin/new-post" class="link linkWhite">New Post</nuxt-link>
          </Intro>
          <nuxt />
       </div>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,9 @@
+<template>
+   <newPostForm />
+</template>
+<script>
+import newPostForm from "@/components/admin/NewPostForm.vue";
+export default {
+   components: { newPostForm },
+};
+</script>
