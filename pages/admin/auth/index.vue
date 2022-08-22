bfa3a8d8f39cf231b1939c7bbc3d5c085882e95c
Nazar Koval <nazarkoval04@gmail.com>
2022-08-23 02:45:30 +0300

    added: styles for auth inputs

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -2,8 +2,10 @@
    <section class="auth">
       <div class="container">
          <form @submit.prevent="onSubmit" class="auth-form">
-            <AppInput>Login:</AppInput>
-            <AppInput>Password:</AppInput>
+            <AppInput type="email" v-model="user.email">Login:</AppInput>
+            <AppInput type="password" v-model="user.password"
+               >Password:</AppInput
+            >
             <div class="controls">
                <AppButton>Log in!</AppButton>
             </div>
@@ -14,10 +16,31 @@
 
 <script>
 export default {
+   layout: "admin",
    data() {
       return {
-         
-      }
+         user: {
+            email: "",
+            password: "",
+         },
+      };
    },
 };
 </script>
+
+<style lang="scss">
+.auth {
+   text-align: center;
+}
+.auth-form {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+   width: 100%;
+   height: 80vh;
+   input {
+      min-width: 460px;
+   }
+}
+</style>
