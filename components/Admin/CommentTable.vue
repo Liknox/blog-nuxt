1a7359a816eaa1fd1444cf2be98284aa4fcd52fd
Nazar Koval <nazarkoval04@gmail.com>
2022-08-22 18:54:41 +0300

    added: CommentTable

---
+++ components/Admin/CommentTable.vue
@@ -0,0 +1,56 @@
+<template>
+   <section>
+      <div class="container">
+         <table>
+            <!-- thead -->
+            <thead>
+               <tr>
+                  <th v-for="(th, index) in thead" :key="index">{{ th }}</th>
+               </tr>
+            </thead>
+            <!-- tbody -->
+            <slot name="tbody"> </slot>
+         </table>
+      </div>
+   </section>
+</template>
+
+<script>
+export default {
+   props: {
+      thead: {
+         type: Array,
+         required: true,
+      },
+   },
+};
+</script>
+
+<style lang="scss" scoped>
+// Default table
+table {
+   width: 100%;
+   line-height: 1.5em;
+   // mb
+   border-collapse: separate;
+   border-spacing: 0 14px;
+   thead th {
+      padding: 20px 22px;
+      white-space: nowrap;
+      font-size: 20px;
+   }
+   td {
+      padding: 20px;
+   }
+   tbody tr {
+      background-color: #ffffff;
+      transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+      box-shadow: 0 16px 16px rgba(0, 0, 0, 0.01);
+      &:hover {
+         box-shadow: 0 16px 16px rgba(0, 0, 0, 0.02);
+         transform: translate(0, -3px);
+         transition-delay: 0s !important;
+      }
+   }
+}
+</style>
