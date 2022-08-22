42436a6987ee3adeb58048ac9e7f99dfed9e7ada
Nazar Koval <nazarkoval04@gmail.com>
2022-08-22 19:50:43 +0300

    added: table for comments (edit only for admins)

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -5,6 +5,7 @@
          <Intro title="Admin Page">
             <nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
             <nuxt-link to="/admin/new-post" class="link linkWhite">New Post</nuxt-link>
+            <nuxt-link to="/admin/comments" class="link linkWhite">Comments</nuxt-link>
          </Intro>
          <nuxt />
       </div>
---
+++ pages/admin/comments/index.vue
@@ -0,0 +1,51 @@
+<template>
+   <commentTable :thead="['Name', 'Text', 'Status', 'Change Status', 'Delete']">
+      <tbody slot="tbody">
+         <tr v-for="comment in comments" :key="comment.id">
+            <td>
+               <span>{{ comment.name }}</span>
+            </td>
+            <td>
+               <span>{{ comment.text }}</span>
+            </td>
+            <td>
+               <span v-if="comment.status" class="status true">Publish</span>
+               <span v-else class="status false">Hiden</span>
+            </td>
+            <td>
+               <span @click="changeComment(comment.id)" class="link">Change Status</span>
+            </td>
+            <td>
+               <span @click="deleteComment(comment.id)" class="link">Delete</span>
+            </td>
+         </tr>
+      </tbody>
+   </commentTable>
+</template>
+<script>
+import commentTable from "@/components/Admin/CommentTable.vue";
+export default {
+   components: { commentTable },
+   layout: "admin",
+   data() {
+      return {
+         comments: [
+            {
+               id: 1,
+               name: "Alex",
+               text: "Lorem ipsum dolor sit amet, consectetur",
+               status: true,
+            },
+            {
+               id: 2,
+               name: "Sanya",
+               text: "Lorem ipsum dolor sit amet, consectetur",
+               status: false,
+            },
+         ],
+      };
+   },
+   methods: {
+   },
+};
+</script>
