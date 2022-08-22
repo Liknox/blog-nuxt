bb8dd633d55ae482074632f4c9865001166cfc31
Nazar Koval <nazarkoval04@gmail.com>
2022-08-23 02:38:57 +0300

    created: auth for admins

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,23 @@
+<template>
+   <section class="auth">
+      <div class="container">
+         <form @submit.prevent="onSubmit" class="auth-form">
+            <AppInput>Login:</AppInput>
+            <AppInput>Password:</AppInput>
+            <div class="controls">
+               <AppButton>Log in!</AppButton>
+            </div>
+         </form>
+      </div>
+   </section>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         
+      }
+   },
+};
+</script>
