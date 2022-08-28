d15dbcad8886264c6bc6ea0a684ef28e2d15146a
Nazar Koval <92331274+Liknox@users.noreply.github.com>
2022-08-28 23:58:43 +0300

    Koval (#15)
    * koval: created capability to edit posts for admin
    
    * koval: created post editing

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,28 +1,42 @@
 <template>
-   <newPostForm :postEdit="post" @submit="onSubmit" />
+	<newPostForm :postEdit="post" @submit="onSubmit" />
 </template>
 <script>
-import newPostForm from "@/components/Admin/NewPostForm.vue";
+import axios from "axios"
+import newPostForm from "@/components/Admin/NewPostForm.vue"
 export default {
-   components: { newPostForm },
-   layout: "admin",
-   data() {
-      return {
-         post: {
-            id: 1,
-            title: "1 post",
-            descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            content:
-               "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            img: "https://images.newscientist.com/wp-content/uploads/2022/02/24145845/SEI_89826778.jpg?crop=1:1,smart&width=1200&height=1200&upscale=true",
-         },
-      };
-   },
-   methods: {
-      onSubmit(post) {
-         console.log("Post Editing!");
-         console.log(post);
-      },
-   },
-};
+	components: { newPostForm },
+	layout: "admin",
+	// data() {
+	//    return {
+	//       post: {
+	//          id: 1,
+	//          title: "1 post",
+	//          descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+	//          content:
+	//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+	//          img: "https://images.newscientist.com/wp-content/uploads/2022/02/24145845/SEI_89826778.jpg?crop=1:1,smart&width=1200&height=1200&upscale=true",
+	//       },
+	//    };
+	// },
+	asyncData(contex) {
+		return axios
+			.get(`https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/posts/${contex.params.postId}.json`)
+			.then((res) => {
+				return {
+					post: { ...res.data, id: contex.params.postId },
+				}
+			})
+			.catch((e) => contex.error(e))
+	},
+	methods: {
+		onSubmit(post) {
+			console.log("Post Editing!")
+         this.$store.dispatch("editPost", post)
+         .then(() => {
+            this.$router.push("/admin")
+         })
+		},
+	},
+}
 </script>
