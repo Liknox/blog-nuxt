52626ffee110dfc954825db37e4496184e9602d1
Nazar Koval <nazarkoval04@gmail.com>
2022-08-29 20:49:11 +0300

    koval: display if comments are publish

--- pages/admin/comments/index.vue
+++ pages/admin/comments/index.vue
@@ -1,61 +1,57 @@
 <template>
-   <commentTable :thead="['Name', 'Text', 'Status', 'Change Status', 'Delete']">
-      <tbody slot="tbody">
-         <tr v-for="comment in comments" :key="comment.id">
-            <td>
-               <span>{{ comment.name }}</span>
-            </td>
-            <td>
-               <span>{{ comment.text }}</span>
-            </td>
-            <td>
-               <span v-if="comment.status" class="status true">Publish</span>
-               <span v-else class="status false">Hiden</span>
-            </td>
-            <td>
-               <span @click="changeComment(comment.id)" class="link"
-                  >Change Status</span
-               >
-            </td>
-            <td>
-               <span @click="deleteComment(comment.id)" class="link"
-                  >Delete</span
-               >
-            </td>
-         </tr>
-      </tbody>
-   </commentTable>
+	<no-ssr>
+		<commentTable :thead="['Name', 'Text', 'Status', 'Change Status', 'Delete']">
+			<tbody slot="tbody">
+				<tr v-for="comment in comments" :key="comment.id">
+					<td>
+						<span>{{ comment.name }}</span>
+					</td>
+					<td>
+						<span>{{ comment.text }}</span>
+					</td>
+					<td>
+						<span v-if="comment.publish" class="status true">Publish</span>
+						<span v-else class="status false">Hiden</span>
+					</td>
+					<td>
+						<span @click="changeComment(comment.id)" class="link">Change Status</span>
+					</td>
+					<td>
+						<span @click="deleteComment(comment.id)" class="link">Delete</span>
+					</td>
+				</tr>
+			</tbody>
+		</commentTable>
+	</no-ssr>
 </template>
 <script>
-import commentTable from "@/components/Admin/CommentTable.vue";
+import axios from "axios"
+import commentTable from "@/components/Admin/CommentTable.vue"
 export default {
-   components: { commentTable },
-   layout: "admin",
-   data() {
-      return {
-         comments: [
-            {
-               id: 1,
-               name: "Alex",
-               text: "Lorem ipsum dolor sit amet, consectetur",
-               status: true,
-            },
-            {
-               id: 2,
-               name: "Sanya",
-               text: "Lorem ipsum dolor sit amet, consectetur",
-               status: false,
-            },
-         ],
-      };
-   },
-   methods: {
-      changeComment(id) {
-         console.log(`Change comment id - ${id}`);
-      },
-      deleteComment(id) {
-         console.log(`Delete comment id - ${id}`);
-      },
-   },
-};
+	components: { commentTable },
+	layout: "admin",
+	data() {
+		return {
+			comments: [],
+		}
+	},
+	created() {
+		axios.get("https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/comments.json").then((res) => {
+			let commentsArray = []
+			Object.keys(res.data).forEach((key) => {
+				const comment = res.data[key]
+				commentsArray.push({ ...comment, id: key })
+			})
+			this.comments = commentsArray
+		})
+	},
+	methods: {
+		changeComment(id) {
+			console.log(`Change comment id - ${id}`)
+		},
+		deleteComment(id) {
+			console.log(`Delete comment id - ${id}`)
+		},
+	},
+}
 </script>
