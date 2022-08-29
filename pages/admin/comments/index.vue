08fa8ca93f75fa17776224dff4571ca0811c5e41
Nazar Koval <nazarkoval04@gmail.com>
2022-08-29 21:11:27 +0300

    koval: created capability to manage comments

--- pages/admin/comments/index.vue
+++ pages/admin/comments/index.vue
@@ -14,7 +14,7 @@
 						<span v-else class="status false">Hiden</span>
 					</td>
 					<td>
-						<span @click="changeComment(comment.id)" class="link">Change Status</span>
+						<span @click="changeComment(comment)" class="link">Change Status</span>
 					</td>
 					<td>
 						<span @click="deleteComment(comment.id)" class="link">Delete</span>
@@ -35,22 +35,28 @@ export default {
 			comments: [],
 		}
 	},
-	created() {
-		axios.get("https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/comments.json").then((res) => {
-			let commentsArray = []
-			Object.keys(res.data).forEach((key) => {
-				const comment = res.data[key]
-				commentsArray.push({ ...comment, id: key })
-			})
-			this.comments = commentsArray
-		})
+	mounted() {
+		this.loadComments()
 	},
 	methods: {
-		changeComment(id) {
-			console.log(`Change comment id - ${id}`)
+		loadComments() {
+			axios.get("https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/comments.json").then((res) => {
+				let commentsArray = []
+				Object.keys(res.data).forEach((key) => {
+					const comment = res.data[key]
+					commentsArray.push({ ...comment, id: key })
+				})
+				this.comments = commentsArray
+			})
+		},
+		changeComment(comment) {
+			comment.publish = !comment.publish
+			axios.put(`https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/comments/${comment.id}.json`, comment)
 		},
 		deleteComment(id) {
-			console.log(`Delete comment id - ${id}`)
+			axios.delete(`https://blog-nuxt-c3be8-default-rtdb.europe-west1.firebasedatabase.app/comments/${id}.json`).then((res) => {
+				this.loadComments()
+			})
 		},
 	},
 }
