e3c0d3a24479ffd386e1630de6e02500cca78fec
Nazar Koval <92331274+Liknox@users.noreply.github.com>
2022-09-03 01:00:52 +0300

    koval: created new dispatch for admin panel

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,6 +5,9 @@
 <script>
 export default {
 	layout: "admin",
+	mounted() {
+		this.$store.dispatch("nuxtServerInit")
+	},
 	computed: {
 		postsLoaded() {
 			return this.$store.getters.getPostsLoaded
