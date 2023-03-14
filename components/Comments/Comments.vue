db57e491f35d1762b9a49552b5162c5313216348
Nazar Koval <n.kovaldev@gmail.com>
2023-03-14 05:41:28 +0200

    update comments title

--- components/Comments/Comments.vue
+++ components/Comments/Comments.vue
@@ -1,7 +1,7 @@
 <template>
 	<section class="comments" v-if="comments">
 		<div class="container">
-			<h2 class="title">No comments yet:</h2>
+			<h2 class="title">Comments: </h2>
 			<p v-if="comments.length === 0">Comments: 0</p>
 			<div class="comment" v-for="comment in comments" :key="comment.name">
 				<p class="name">{{ comment.name }}</p>
--- components/Promo.vue
+++ components/Promo.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="promo">
-        <div class="container">
-            <h1 class="title">My SSR Blog! With Nuxt.js!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, id sequi! Officiis harum fuga eos obcaecati eius sed temporibus corporis?</p>
-        </div>
-    </section>
+	<section class="promo">
+		<div class="container">
+			<h1 class="title">My SSR Blog! With Nuxt.js!</h1>
+			<p>With rendering and admin panel, full SEO optimization and a small CRM system inside!</p>
+		</div>
+	</section>
 </template>
