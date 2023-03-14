542da19b2704496bc7adce04b85eec85de465533
Nazar Koval <n.kovaldev@gmail.com>
2023-03-14 06:50:39 +0200

    deploying

--- components/Promo.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<section class="promo">
-		<div class="container">
-			<h1 class="title">My SSR Blog! With Nuxt.js!</h1>
-			<p>With rendering and admin panel, full SEO optimization and a small CRM system inside!</p>
-		</div>
-	</section>
-</template>
--- middleware/auth.js
+++
@@ -1,7 +0,0 @@
-export default function (contex) {
-   if (!contex.store.getters.checkAuthUser){ 
-      contex.redirect("/admin/auth")
-   }
-   console.log(contex);
-}
-
